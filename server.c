92cc0ce52f4f0263b1102424d1706133408854b7
Leo-aab <leonardo45abreu@gmail.com>
2026-04-20 21:49:45 -0300

    defining ipv4 and port

--- server.c
+++ server.c
@@ -17,4 +17,12 @@ int main() {
       send_data[1024]; // buffer to store incoming data from the client
   struct sockaddr_in server_addr,
       client_addr; // Structures to hold server and client IP/PORT
+
+  sock = socket(AF_INET, SOCK_STREAM, 0); // AF_INET = IPV4 | SOCK_STREAM = UDP
+  if (sock == -1) {
+    perror("Socket error");
+    exit(1);
+  }
+  server_addr.sin_family = AF_INET;   // defining ipv4
+  server_addr.sin_port = htons(5000); // defining port
 }
