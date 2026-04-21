9beb5886e642763087f8004a367eca4df3ab23ec
Leo-aab <leonardo45abreu@gmail.com>
2026-04-20 21:33:26 -0300

    initializing udp connection

---
+++ server.c
@@ -0,0 +1,16 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+  int sock;
+  int addr_len, bytes_read;
+  char recv_data[1024], send_data[1024];
+  struct sockaddr_in server_addr, client_addr;
+}
