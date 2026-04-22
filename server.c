fa440ea9dd0d910ba24f497c1d0e6a1358e25abb
Leo-aab <leonardo45abreu@gmail.com>
2026-04-22 11:07:18 -0300

    changing binding format, adding memset to zero the struct chars and starting a loop for server

--- server.c
+++ server.c
@@ -7,6 +7,9 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+//header for posix sockets
+
+
 
 int main() {
   // adding vars
@@ -20,18 +23,21 @@ int main() {
 
   sock = socket(AF_INET, SOCK_DGRAM, 0); // AF_INET = IPV4 | SOCK_DGRAM = UDP
   if (sock == -1) {
-    perror("Socket error");
+    perror("Socket error"); //pererror = errno to print the error thats causing
     exit(1);
   }
+    memset(&server_addr, 0, sizeof(server_addr)); //zero bytes before define ip and port
   server_addr.sin_family = AF_INET;   // defining ipv4
   server_addr.sin_port = htons(5000); // defining port
   server_addr.sin_addr.s_addr =
       INADDR_ANY; // INADDR_ANY - to bind to all interfaces
 
-  if(memset(&(server_addr.sin_zero), 0, 8) < 0){
+  if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) < 0) {
     perror("bind");
     return 1;
   } // binding
 
-
+   while (1){
+     bytes_read = recvfrom()
+   }
 }
